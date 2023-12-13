b10743c2a66c21a2102fe26462144e8046abbd13
Lazaros Psarokostas <lazarospsa@hotmail.com>
2023-12-13 23:53:53 +0200

    gofmt example

--- example/main.go
+++ example/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-        "fmt"
-        gofin "github.com/lazarospsa/gofin"
+	"fmt"
+	gofin "github.com/lazarospsa/gofin"
 )
 
 func main() {
-        fmt.Println(gofin.FutureValue(1000, 0.05, 10))
-        fmt.Println(gofin.PresentValue(1000, 0.05, 10))
+	fmt.Println(gofin.FutureValue(1000, 0.05, 10))
+	fmt.Println(gofin.PresentValue(1000, 0.05, 10))
 }
